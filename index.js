01e17dbe8aa0f0bbcd7aae250fa21be06c4ca86e
tomcorey26 <tomcorey26@gmail.com>
2018-05-24 20:42:42 -0400

    displays when user connects

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-    });
-  });
-
-  io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
       io.emit('chat message', msg);
     });
+    io.emit('uconnect');
   });
